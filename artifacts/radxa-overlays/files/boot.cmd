fcec3ce57f6a031bf9324e2f08f99059d6d41776
Enrique Hernández Bello <ehbello@gmail.com>
2024-10-27 16:03:07 +0000

    feat: add new package radxa-overlays

---
+++ artifacts/radxa-overlays/files/boot.cmd
@@ -0,0 +1,22 @@
+setenv soc_vendor "rockchip"
+
+echo "Boot script loaded from ${devtype} ${devnum}"
+
+if test -e ${devtype} ${devnum}:${distro_bootpart} ${prefix}boot.env; then
+	load ${devtype} ${devnum}:${distro_bootpart} ${fdtoverlay_addr_r} ${prefix}boot.env
+	env import -t ${fdtoverlay_addr_r} ${filesize}
+fi
+
+echo "Applying base device tree: ${fdtfile}"
+load ${devtype} ${devnum}:${distro_bootpart} ${fdt_addr_r} ${prefix}dtb/${fdtfile}
+fdt addr ${fdt_addr_r}
+fdt resize 65536
+
+for overlay in ${overlays}; do
+    if load ${devtype} ${devnum}:${distro_bootpart} ${fdtoverlay_addr_r} ${prefix}dtb/${soc_vendor}/overlays/${overlay}.dtbo; then
+        echo "Applying DT overlay: $overlay"
+        fdt apply ${fdtoverlay_addr_r}
+    fi
+done
+
+bootefi bootmgr ${fdt_addr_r}
